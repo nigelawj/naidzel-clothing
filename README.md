2d6a2cb7eff10f07f9a405fb9eec70b7bf0c88c5
Nigel <nigelawj@gmail.com>
2020-06-18 18:28:04 +0800

    added service worker and express-sslify; TBC

---
+++ README.md
@@ -0,0 +1,10 @@
+### Test ReactJS Project
+Hosted on [heroku](https://naidzel-clothing-live.herokuapp.com/)
+- Simple test clothing store
+- Firebase DB
+- Stripe payment
+
+### Future Improvements
+- Implement mobile responsiveness
+- Implement loading of components in chunks via React Lazy and Suspense
+- Write tests with Jest/Enzyme
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const enforce = require('express-sslify');
+const compression = require('compression');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
@@ -13,11 +15,13 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(compression);
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
   app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
@@ -28,6 +32,10 @@ app.listen(port, (err) => {
     throw err;
   }
   console.log(`Server running on port ${port}`);
+});
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 
 app.post('/payment', (req, res) => {
